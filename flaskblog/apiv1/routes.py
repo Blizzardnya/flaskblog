443a6b1b69f3a9f6287819fa2e05be3a8172090e
blizzardnya <e.hmelnikov@yandex.by>
2020-06-26 15:11:04 +0300

    Add post rest api

--- flaskblog/apiv1/routes.py
+++ flaskblog/apiv1/routes.py
@@ -1,14 +1,18 @@
 from datetime import datetime
 
 from flask import Blueprint, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_login import current_user, login_required
 
 from flaskblog import db
-from flaskblog.models import Post
+from flaskblog.models import Post, User
 
 apiv1 = Blueprint('apiv1', __name__)
 api = Api(apiv1, prefix='/api/1')
+
+posts_parser = reqparse.RequestParser()
+posts_parser.add_argument('user', type=str)
+posts_parser.add_argument('count', type=int, required=False, help='Count of last records')
 
 
 class PostApi(Resource):
@@ -21,7 +25,6 @@ class PostApi(Resource):
         return {'post': {'title': post.title, 'date_posted': str(post.date_posted),
                          'content': post.content}}
 
-    @login_required
     def post(self):
         try:
             data = request.get_json()
@@ -35,4 +38,23 @@ class PostApi(Resource):
             return {'message': f'Error with text: {str(Exception)}'}, 400
 
 
-api.add_resource(PostApi, '', '/post/<int:post_id>')
+class PostsByUserApi(Resource):
+    def get(self):
+        args = posts_parser.parse_args()
+        user = User.query.filter_by(username=args['user']).first()
+        count = args['count']
+
+        if not user:
+            return {'message': 'User is not found'}, 400
+
+        output = []
+
+        for post in user.posts if not count else user.posts[::-1][:count]:
+            output.append({'id': post.id, 'title': post.title, 'date_posted': str(post.date_posted),
+                           'content': post.content})
+
+        return {'posts': output}
+
+
+api.add_resource(PostApi, '/post/<int:post_id>')
+api.add_resource(PostsByUserApi, '/posts')
