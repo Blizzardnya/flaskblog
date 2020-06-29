ed25bb0df89a2d5ea86dcb34c7fe799956e3608a
blizzardnya <e.hmelnikov@yandex.by>
2020-06-29 11:31:49 +0300

    Add jwt access token login Create post by REST API

--- flaskblog/apiv1/routes.py
+++ flaskblog/apiv1/routes.py
@@ -2,9 +2,9 @@ from datetime import datetime
 
 from flask import Blueprint, request
 from flask_restful import Resource, Api, reqparse
-from flask_login import current_user, login_required
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
-from flaskblog import db
+from flaskblog import db, bcrypt
 from flaskblog.models import Post, User
 
 apiv1 = Blueprint('apiv1', __name__)
@@ -15,7 +15,7 @@ posts_parser.add_argument('user', type=str)
 posts_parser.add_argument('count', type=int, required=False, help='Count of last records')
 
 
-class PostApi(Resource):
+class PostByIdApi(Resource):
     def get(self, post_id):
         post = Post.query.filter_by(id=post_id).first()
 
@@ -25,17 +25,35 @@ class PostApi(Resource):
         return {'post': {'title': post.title, 'date_posted': str(post.date_posted),
                          'content': post.content}}
 
+
+class CreatePostApi(Resource):
+    @jwt_required
     def post(self):
         try:
-            data = request.get_json()
+            email = get_jwt_identity()
+            user = User.query.filter_by(email=email).first()
 
-            db.session.add(Post(title=data['title'], date_posted=datetime.now(), content=data['content'],
-                                user_id=current_user))
+            if not user:
+                return {'message': 'User is not found'}, 400
+
+            if not request.is_json:
+                return {"msg": "Missing JSON in request"}, 400
+
+            title = request.json.get('title', None)
+            content = request.json.get('content', None)
+
+            if not title:
+                return {"msg": "Missing title parameter"}, 400
+            if not content:
+                return {"msg": "Missing content parameter"}, 400
+
+            db.session.add(Post(title=title, date_posted=datetime.now(), content=content,
+                                user_id=user.id))
             db.session.commit()
 
             return {'message': 'Post has been created'}, 201
         except Exception:
-            return {'message': f'Error with text: {str(Exception)}'}, 400
+            return {'message': f'Error'}, 400
 
 
 class PostsByUserApi(Resource):
@@ -56,5 +74,29 @@ class PostsByUserApi(Resource):
         return {'posts': output}
 
 
-api.add_resource(PostApi, '/post/<int:post_id>')
+class LoginApi(Resource):
+    def post(self):
+        if not request.is_json:
+            return {"msg": "Missing JSON in request"}, 400
+
+        email = request.json.get('email', None)
+        password = request.json.get('password', None)
+
+        if not email:
+            return {"msg": "Missing email parameter"}, 400
+        if not password:
+            return {"msg": "Missing password parameter"}, 400
+
+        user = User.query.filter_by(email=email).first()
+
+        if user and bcrypt.check_password_hash(user.password, password):
+            access_token = create_access_token(identity=email)
+            return {'access_token': access_token}, 200
+        else:
+            return {"msg": "Bad email or password"}, 401
+
+
+api.add_resource(PostByIdApi, '/post/<int:post_id>')
+api.add_resource(CreatePostApi, '/post')
 api.add_resource(PostsByUserApi, '/posts')
+api.add_resource(LoginApi, '/login')
